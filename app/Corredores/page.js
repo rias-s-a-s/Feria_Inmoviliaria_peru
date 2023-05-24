0a88d7111bfc3fe8f4e4d45db700e28d72bd2129
jhonned01 <jhonned01@gmail.com>
2023-05-24 11:49:36 -0500

    projectos

--- app/Corredores/page.js
+++ app/Corredores/page.js
@@ -1,9 +1,31 @@
+"use client";
 import SeccionPage from "../SeccionPage";
+import ItemTeam from "./ItemTeam";
 
 const Corredores = () => {
   return (
     <div>
       <SeccionPage title={"Corredores"} />
+
+      {/* component */}
+      <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+        <div className="text-center pb-12">
+          <h2 className="text-base font-bold text-indigo-600">
+            Disponemos de los mejores equipos del mercado{" "}
+          </h2>
+          <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+            Conoce a los miembros de nuestro corredores{" "}
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <ItemTeam />
+          <ItemTeam />
+          <ItemTeam />
+          <ItemTeam />
+          <ItemTeam />
+          <ItemTeam />
+        </div>
+      </section>
     </div>
   );
 };
